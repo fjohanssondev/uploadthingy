0297ef6bd31a9095d703b9de3628a81abc114bb8
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2023-05-27 23:07:45 +0200

    Things...

--- src/app/components/Upload.tsx
+++ src/app/components/Upload.tsx
@@ -4,23 +4,21 @@
 import "@uploadthing/react/styles.css";
  
 import { UploadButton } from "@uploadthing/react";
-import { OurFileRouter } from "@/app/api/uploadthing/core"
+import { OurFileRouter } from "@/api/uploadthing/core"
  
-export default function Home() {
+export default function Upload() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UploadButton<OurFileRouter>
-        endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          // Do something with the response
-          console.log("Files: ", res);
-          alert("Upload Completed");
-        }}
-        onUploadError={(error: Error) => {
-          // Do something with the error.
-          alert(`ERROR! ${error.message}`);
-        }}
-      />
-    </main>
+    <UploadButton<OurFileRouter>
+      endpoint="imageUploader"
+      onClientUploadComplete={(res) => {
+        // Do something with the response
+        console.log("Files: ", res);
+        alert("Upload Completed");
+      }}
+      onUploadError={(error: Error) => {
+        // Do something with the error.
+        alert(`ERROR! ${error.message}`);
+      }}
+    />
   );
 }
